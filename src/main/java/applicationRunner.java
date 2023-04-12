78122e9aa19e0820c1d408696c8d4d5b22b3d0b7
JeremyMcCray <Jeremy.mccray1337@gmail.com>
2023-04-12 05:05:05 -0400

    add player/card/board/basic logging

--- src/main/java/applicationRunner.java
+++ src/main/java/applicationRunner.java
@@ -1,7 +1,36 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class applicationRunner {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        Gameboard gameboard = new Gameboard();
+        gameboard.userPlayer = new Player();
+        gameboard.enemyPlayer = new Player();
+
+        while (gameboard.isPlayerAlive() && gameboard.isEnemyAlive()) {
+            /* GAME LOOP */
+            // player's turn
+            System.out.println("PLAYER'S TURN");
+
+            // check for victory
+            if (!gameboard.hasPlayerWon()) {
+                // enemy's turn
+                System.out.println("ENEMY'S TURN");
+            }
+        }
+
+        System.out.println("GAME OVER!");
+        if (gameboard.isPlayerAlive()) {
+            System.out.println("YOU WIN!");
+        } else {
+            System.out.println("YOU LOSE!");
+        }
+    }
+
+    public void displayHand(ArrayList<Card> hand) {
+        for (Card card: hand) {
+        }
     }
 }
